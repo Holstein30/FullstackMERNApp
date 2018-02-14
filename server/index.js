d68b3240f8298acb97e43e3c2e59cbb0c3519ddf
Dale Holstein <DaleHolstein@Dales-MacBook-Pro.local>
2018-02-14 18:37:02 -0500

    fixed

--- server/index.js
+++ server/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send({ hi: 'there' });
 });
 
-app.listen("Running on Port: " + PORT);
+app.listen(PORT);
